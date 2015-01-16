c6fd997be2c06fe0ea977b0480164f38d9dce13c
Sebastian Thiel <byronimo@gmail.com>
2015-01-16 12:38:25 +0100

    Added some more code to actually download a given URL and print it to stdout

--- src/c/lc_download.c
+++ src/c/lc_download.c
@@ -13,11 +13,31 @@ Usage:
 #include <stdio.h>
 #include "curl/curl.h"
 
-int main() {
+int main(int argc, const char* args[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Please provide exactly one URL to print to standard output\n");
+		return 1;
+	}
+
 	CURL* curl = curl_easy_init();
 	if (!curl) {
-		fprintf(stderr, "curl initialization failure");
+		fprintf(stderr, "curl initialization failure\n");
 		return 128;
+	}
+
+	CURLcode res = CURLE_OK;
+	if ((res = curl_easy_setopt(curl, CURLOPT_URL, args[1])) != CURLE_OK) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 3;
+	}
+	if ((res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L) != CURLE_OK)) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 3;
+	}
+
+	if ((res = curl_easy_perform(curl)) != CURLE_OK) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 4;
 	}
 
 	curl_easy_cleanup(curl);
